f49050a7e04401a578fbdfcf228a7c448d086659
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-26 21:03:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+
+
+let table = document.querySelector('table')
 function insert_Row() {
     //Write your code here
-  
-  
+	let tr = document.createElement('tr');
+	let td = document.createElement('td');
+	td.innerText = `Row Cell`;
+	let td = document.createElement('td');
+	td.innerText = `Row Cell`
+	tr.append(td , td);
+	table.append(tr);
+	
 }
